a10384fe464b16b11b84c70af73747da276afe2f
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-02-12 01:29:21 +0600

    Add files via upload

---
+++ method/Classes.java
@@ -0,0 +1,12 @@
+
+public class Classes {
+    String name, gender;
+  int phone ;
+  void displayInformation(){
+      System.out.println("Name:" + name);
+      System.out.println("Gender:" + gender);
+      System.out.println("Phone:" + phone);
+      System.out.println("\n\n");
+  }
+}
+
---
+++ method/Test.java
@@ -0,0 +1,28 @@
+
+    public class Test{
+
+        public static void main(String[] args){
+        Classes teacher1 ;
+        teacher1 = new Classes();
+        teacher1.name= "nirob";
+        teacher1.gender= "male";
+        teacher1.phone= 1752331906 ;
+        teacher1.displayInformation();
+
+
+
+
+            Classes teacher2 ;
+            teacher2 = new Classes();
+            teacher2.name= "hasan";
+            teacher2.gender= "male";
+            teacher2.phone= 1752331906 ;
+            teacher2.displayInformation();
+
+
+
+
+        }
+
+
+    }
