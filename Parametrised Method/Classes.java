f07d1e6ec8dfc66c665690a958e4c5aa2e2c04fc
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-02-12 01:41:12 +0600

    Add files via upload

---
+++ Parametrised Method/Classes.java
@@ -0,0 +1,17 @@
+
+public class Classes {
+    String name, gender;
+  int phone ;
+  void setInformation(String m, String n, int p ){
+name = m;
+gender= n;
+phone = p;
+  }
+  void displayInformation(){
+      System.out.println("Name:" + name);
+      System.out.println("Gender:" + gender);
+      System.out.println("Phone:" + phone);
+      System.out.println("\n\n");
+  }
+}
+
---
+++ Parametrised Method/Test.java
@@ -0,0 +1,24 @@
+
+    public class Test{
+
+        public static void main(String[] args){
+        Classes teacher1 ;
+        teacher1 = new Classes();
+        teacher1.setInformation("mursalin" , "male", 1234567);
+        teacher1.displayInformation();
+
+
+
+
+            Classes teacher2 ;
+            teacher2 = new Classes();
+            teacher2.setInformation("nirob", "male",86543322);
+            teacher2.displayInformation();
+
+
+
+
+        }
+
+
+    }
