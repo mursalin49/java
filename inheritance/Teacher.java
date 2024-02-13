374b6c2d8e2a48a421eb7b1ec82824fc26e22f67
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-02-13 10:39:46 +0600

    Add files via upload

--- inheritance/Teacher.java
+++ inheritance/Teacher.java
@@ -1,8 +1,9 @@
 public class Teacher extends Inheritance{
     String qualification;
     void displyInformation2(){
-        System.out.println("Name :"+name);
-        System.out.println("Age :"+age);
+        displayInformation1();
+       /* System.out.println("Name :"+name);
+        System.out.println("Age :"+age);*/
         System.out.println("Qualification: :"+qualification);
     }
 }
