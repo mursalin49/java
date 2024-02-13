cf5b97e5d3986be4b2f331d96f14a88a25b49c6b
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-02-13 10:38:02 +0600

    Add files via upload

---
+++ inheritance/Teacher.java
@@ -0,0 +1,8 @@
+public class Teacher extends Inheritance{
+    String qualification;
+    void displyInformation2(){
+        System.out.println("Name :"+name);
+        System.out.println("Age :"+age);
+        System.out.println("Qualification: :"+qualification);
+    }
+}
---
+++ inheritance/Test.java
@@ -0,0 +1,14 @@
+
+    public class Test{
+
+        public static void main(String[] args){
+       Teacher t1 = new Teacher();
+      t1.name="nirob";
+      t1.age= 30;
+      t1.qualification= "cse";
+      t1.displyInformation2();
+
+        }
+
+
+    }
