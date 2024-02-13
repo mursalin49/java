d673d226ebd2e1c5b40564d35a2e53f7725ac88a
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-02-13 12:28:06 +0600

    Add files via upload

---
+++ abstract/Karrim.java
@@ -0,0 +1,6 @@
+public   class Karrim extends MobileUser {
+    @Override
+    void sendMessage() {
+        System.out.println("Hi, I am Karim");
+    }
+}
---
+++ abstract/Test.java
@@ -0,0 +1,15 @@
+
+    public class Test{
+
+        public static void main(String[] args){
+      MobileUser mu;
+      mu = new Rahim();
+      mu.sendMessage();
+
+      mu = new Karrim();
+      mu.sendMessage();
+
+        }
+
+
+    }
