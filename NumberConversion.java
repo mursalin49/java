4d26d33c8806e46ac26ead0c930aa126572ff391
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-02-21 00:38:00 +0600

    Add files via upload

---
+++ NumberConversion.java
@@ -0,0 +1,9 @@
+public class NumberConversion {
+    public static void main(String[] args){
+        String binary= "1010";  // binary to decimal
+        Integer decimal = Integer.parseInt(binary,2);  // base 2
+        System.out.println(decimal);
+
+        // same way baki gula
+    }
+}
---
+++ Primitive.java
@@ -0,0 +1,10 @@
+public class Primitive {
+    public static void main(String[] args)
+    {
+     double i = 100.25;
+        String s = Double.toString(i);
+        System.out.println("s = "+s);
+
+
+    }
+}
