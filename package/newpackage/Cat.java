0f2706217b60565329b03c9b207ff97451a03848
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2024-03-21 23:07:54 +0600

    Add files via upload

---
+++ package/newpackage/Cat.java
@@ -0,0 +1,9 @@
+package newpackage;
+
+public class Cat {
+    public static  void main(String[] args){
+Dog dog = new Dog();
+newpackage2.Dog dog1 = new newpackage2.Dog();
+
+    }
+}
---
+++ package/newpackage/Dog.java
@@ -0,0 +1,11 @@
+package newpackage;
+
+import newpackage2.Cow;
+import newpackage2.Donkey;
+
+public class Dog {
+    public  static  void  main(String[] args){
+        Cow cow = new Cow();
+        Donkey donkey = new Donkey();
+    }
+}
